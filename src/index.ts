c9e772cd9577460be66838da5e3c93204c0c4c77
Rahul kardile <rahulkardile321@gmail.com>
2024-07-26 08:21:12 +0530

    user insert query added

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import express, { NextFunction, Request, Response } from "express"
 import dotenv from "dotenv"
 import createTable from "./create-table";
+import { getClient } from "./utils/connectDb";
 
 const app = express();
+
+app.use(express.json());
 
 dotenv.config();
 const PORT = process.env.PORT || 3300;
@@ -12,6 +15,29 @@ const PORT = process.env.PORT || 3300;
 app.get("/", (req: Request, res: Response ,next: NextFunction)=>{
     try {
         res.send("okk");
+    } catch (error) {
+        next(error);
+    }
+})
+
+app.post("/create-user", async(req: Request, res: Response ,next: NextFunction)=>{
+    try {
+
+        const { email, password } = req.body;
+
+        const client = await getClient()
+        const inserUserText = `INSERT INTO users (email, password) VALUES ($1, $2) RETURNING id`;
+        const userValues = [email, password]
+
+        let response = await client.query(inserUserText, userValues);
+
+        console.log(res);
+
+        res.status(200).json({
+            message: "user has been created",
+            data: response
+        })        
+        
     } catch (error) {
         next(error);
     }
